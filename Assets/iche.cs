302ad948345d1da49a6d4a0d35cb9fc60a2bf616
Samsung_Student <damirishm3010@icloud.com>
2022-11-29 17:26:13 +0300

    Update script

--- Assets/iche.cs
+++ Assets/iche.cs
@@ -7,7 +7,7 @@ public class iche : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("A");
     }
 
     // Update is called once per frame
